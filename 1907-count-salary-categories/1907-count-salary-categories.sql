9998bef121c1fc9ecdd622a29bfb90545f7ff61c
Ajay kumar <121481541+Ajay9508@users.noreply.github.com>
2026-05-19 15:40:38 +0530

    Time: 1679 ms (21.29%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,10 +1,20 @@
 # Write your MySQL query statement below
-select 'Low Salary' as category,count(*) as accounts_count from Accounts
-where  income<20000
-union 
-select 'Average Salary' as category, count(*) as accounts_count from Accounts
-where income between 20000 and 50000
-union 
-select 'High Salary' as category,count(*) as accounts_count from Accounts
-where income>50000
-group by category;
+-- select 'Low Salary' as category,count(*) as accounts_count from Accounts
+-- where  income<20000
+-- union 
+-- select 'Average Salary' as category, count(*) as accounts_count from Accounts
+-- where income between 20000 and 50000
+-- union 
+-- select 'High Salary' as category,count(*) as accounts_count from Accounts
+-- where income>50000
+-- group by category;
+-- using join + group by funcation
+select c.category,count(a.account_id)as accounts_count FROM
+(select 'Low Salary' as category union
+select 'Average Salary' as category union
+select 'High Salary' as category) c
+left join Accounts a on (c.category = 'Low Salary' and a.income<20000)or
+(c.category = 'Average Salary' and a.income between 20000 and 50000)or
+(c.category = 'High Salary' and a.income>50000)
+group by c.category;
+-- one more method is case + gruop by
