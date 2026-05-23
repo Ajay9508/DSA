d9221287327779bad1f91d936fd52cf2eb5c4382
Ajay kumar <121481541+Ajay9508@users.noreply.github.com>
2026-05-23 13:42:49 +0530

    Time: 1 ms (89.72%), Space: 44.7 MB (66.01%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+            int len = nums.length;
+            int n= len-1;
+            if(len<2){
+                return false;
+            }
+            int[] count = new int[n+1];
+        for (int num : nums) {
+            if (num > n || num < 1) {
+                return false;
+            }
+            count[num]++;
+        }
+        for(int i=1;i<n;i++){
+            if(count[i]!=1){
+                return false;
+            }
+        }
+     return count[n] == 2;  
+    }
+}
