20e5ef6ca734dce7aa368027eba6cc7add39da2a
Ajay kumar <121481541+Ajay9508@users.noreply.github.com>
2026-05-29 23:40:18 +0530

    Time: 1 ms (71.3%), Space: 42.4 MB (17.68%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isThree(int n) {
+        int divisor =0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                divisor++;
+            }
+            if(divisor>3){
+                return false;
+            }
+        }
+        return divisor ==3;
+    }
+}
